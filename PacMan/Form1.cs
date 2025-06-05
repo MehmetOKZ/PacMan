b4a9da9f265f1dcc729e50166142c4a8f230f83f
MehmetOKZ <levizeithh@gmail.com>
2025-06-05 15:52:10 +0300

    Klavye yön hareketleri, Timer, posiyonunu bulma metotları yazıldı.

--- PacMan/Form1.cs
+++ PacMan/Form1.cs
@@ -31,7 +31,59 @@ namespace PacMan
         }
         private void Form1_Keydown(object sender, KeyEventArgs e) // Klavye yön hareketi metotu
         {
+            switch(e.KeyCode)
+            {
+                case Keys.Left:
+                    yon = "Left";
+                    break;
+                case Keys.Right:
+                    yon = "Right";
+                    break;
+                case Keys.Up:
+                    yon = "Up";
+                    break;
+                case Keys.Down:
+                    yon = "Down";
+                    break;
+            }
+        }
 
+        //timer çalıştıkça bu metot her 100ms de bir çalışır.
+        private void OyunZamanı_Tick(object sender,EventArgs e)
+        {
+            PictureBox pbpacman=Controls.Find("pbpacman",true).FirstOrDefault() as PictureBox;  //pacman karakterini bulan metot
+            if (pbpacman==null)
+            {
+                return;
+            }
+            Point oldPosition=pbpacman.Location; //hareketlerin önceki konumu
+
+            switch(yon) //seçilen yöne pacmanin hareket etmesini sağlar
+            {
+                case "Left":
+                    pbpacman.Left -= 5;
+                    break;
+                case "Right":
+                    pbpacman.Left += 5;
+                    break;
+                case "Up":
+                    pbpacman.Top -= 5;
+                    break;
+                case "Down":
+                    pbpacman.Top += 5;
+                    break;
+            }
+            foreach (Control ctrl in Controls)
+            {
+                if (ctrl is PictureBox && ctrl.Tag?.ToString() == "wall")
+                {
+                    if(pbpacman.Bounds.IntersectsWith(ctrl.Bounds))
+                    {
+                        pbpacman.Location = oldPosition;
+                        break;
+                    }
+                }
+            }
         }
     }
 }
